7ef073c2a51b94d152af3101aaa659f48ae742a5
thi3n <136480142+thiengthb@users.noreply.github.com>
2025-04-29 18:32:12 +0700

    chore(app): config fonts Poppins and Montserrat, update tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <h1>Welcome to Tracknova PSMS Frontend!</h1>
+      <p>This is a placeholder for the main application content.</p>
+    </div>
+  );
 }
 
 export default App;
