e53528c9a721415670e2695fe4c7cc18ae14444f
Nguyen Minh Quang <nguyenminhquangnmqlamdong@gmail.com>
2025-04-30 18:47:56 +0700

    refactor(Navbar): use SVG to handle icon clicked

--- src/components/footer/Navbar.tsx
+++ src/components/footer/Navbar.tsx
@@ -1,13 +1,12 @@
 import { defaultIcons } from "../../helpers/HNav-icon"
-import { boldIcons } from "../../helpers/HNav-icon"
 import { Link } from 'react-router-dom'
 import { useState } from "react"
-
+import React from 'react'
 /*
     This is footer navbar of the app
     including 5 icons that will navigate the user
     to other pages
-*/ 
+*/
 function Navbar() {
     // using state to set active icon to the current page
     const [activeIndex, setActiveIndex] = useState(0);
@@ -21,26 +20,28 @@ function Navbar() {
     ]
 
 
-    const handleClikec = (index: number) => {
-        setActiveIndex(index)
-    }
-
     return (
         <>
             <ul className="navbar w-full flex justify-around p-5 bg-[#F5FFFF] rounded-[30px]">
                 {
-                    navLinks.map((link, index) => (
-                        <li className="nav-item" key={index}>
-                            <Link to={link}>
-                                {/* check the clicked img is based on which bold icon */}
-                                <img className="w-[25px] h-[25px]" 
-                                    src = {activeIndex === index ? boldIcons[index].src : defaultIcons[index].src} 
-                                    alt = {activeIndex === index ? boldIcons[index].alt : defaultIcons[index].alt} 
-                                    onClick={() => handleClikec(index)}
-                                    />
-                            </Link>
-                        </li>
-                    ))
+                    navLinks.map((link, index) => {
+                        // clone the svg to add more style when it is clicked
+                        const styleIcon = React.cloneElement(defaultIcons[index], {
+                            onClick: () => setActiveIndex(index),
+                            style: {
+                                color: activeIndex === index ? "#0093CB": "#6DD7FD"
+                            },
+                            className: "transition duration-300 ease-in-out hover:scale-120"
+                        })
+
+                        return (
+                            <li className="nav-item" key={index}>
+                                <Link to={link}>
+                                    {styleIcon}
+                                </Link>
+                            </li>
+                        )
+                    })
                 }
             </ul>
         </>
