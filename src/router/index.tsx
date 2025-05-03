b94d732aba4d328f2273e4acbe996420ded7c56b
Thien <tranngocthien628@gmail.com>
2025-05-04 03:45:30 +0700

    fix(app): Struct router file to touter folder and change name to index.tsx

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,20 +1,20 @@
 import { createBrowserRouter } from 'react-router-dom';
-import App from './App';
-import LoginPage from './pages/authen/LoginPage.tsx';
-import HomePage from './pages/HomePage.tsx';
-import InvoicesPage from './pages/invoice/InvoicesPage.tsx';
-import InvoiceDetailsPage from './pages/invoice/InvoiceDetailsPage.tsx';
-import OrderDetailsPage from './pages/order/OrderDetailsPage.tsx';
-import OrderTransactionsPage from './pages/transaction/OrderTransactionsPage.tsx';
-import ProductsPage from './pages/product/ProductsPage.tsx';
-import NewProductPage from './pages/product/NewProductPage.tsx';
-import ProductDetailsPage from './pages/product/ProductDetailsPage.tsx';
-import CustomersPage from './pages/customer/CustomersPage.tsx';
-import CustomerDetailsPage from './pages/customer/CustomerDetailsPage.tsx';
-import MenuPage from './pages/menu/MenuPage.tsx';
-import EditMenuPage from './pages/menu/EditMenuPage.tsx';
-import NewOrderPage from './pages/order/NewOrderPage.tsx';
-import NotFoundPage from './pages/NotFoundPage.tsx';
+import App from '../App.tsx';
+import LoginPage from '../pages/authen/LoginPage.tsx';
+import HomePage from '../pages/HomePage.tsx';
+import InvoicesPage from '../pages/invoice/InvoicesPage.tsx';
+import InvoiceDetailsPage from '../pages/invoice/InvoiceDetailsPage.tsx';
+import OrderDetailsPage from '../pages/order/OrderDetailsPage.tsx';
+import OrderTransactionsPage from '../pages/transaction/OrderTransactionsPage.tsx';
+import ProductsPage from '../pages/product/ProductsPage.tsx';
+import NewProductPage from '../pages/product/NewProductPage.tsx';
+import ProductDetailsPage from '../pages/product/ProductDetailsPage.tsx';
+import CustomersPage from '../pages/customer/CustomersPage.tsx';
+import CustomerDetailsPage from '../pages/customer/CustomerDetailsPage.tsx';
+import MenuPage from '../pages/menu/MenuPage.tsx';
+import EditMenuPage from '../pages/menu/EditMenuPage.tsx';
+import NewOrderPage from '../pages/order/NewOrderPage.tsx';
+import NotFoundPage from '../pages/NotFoundPage.tsx';
 
 const router = createBrowserRouter([
   { path: '/', element: <App /> },
