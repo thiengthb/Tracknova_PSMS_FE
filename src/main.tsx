751e7b5364f0f1528aa21aaeb434b69d2081e887
Thien <tranngocthien628@gmail.com>
2025-05-04 01:45:56 +0700

    feat(app): configure router in the main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css'
 
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  { path: '/home', element: <App /> },
+])
+
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
     <StrictMode>
-      <App />
+      <RouterProvider router={router} />
     </StrictMode>
-  </BrowserRouter>,
 )
