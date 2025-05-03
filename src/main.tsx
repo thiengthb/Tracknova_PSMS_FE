5c2b74538517b92a4bfb2bdfa1b6f89735b5cdd6
Thien <tranngocthien628@gmail.com>
2025-05-04 03:48:42 +0700

    fix(main): fix main file import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom';
 import './index.css'
-import router from './router/router.tsx';
+import router from './router/index.tsx';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
